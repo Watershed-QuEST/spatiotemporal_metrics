b713215751b79f7221c727b771e6df599648b22f
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2026-09-09 12:22:00 -0700

    Importing data
    Toy data set import code copied over from synchrony

--- VarianceCollapse.R
+++ VarianceCollapse.R
@@ -1,7 +1,22 @@
 ## Variance collapse code demonstration
-## Drafted by Joanna Blaszczak
+## Drafted by J.R. Blaszczak
+
+## Import Packages
 
 
+#### Imports ####
+# List all files in the folder
+toy_files <- drive_ls(drive_get("https://drive.google.com/drive/u/1/folders/1zh0YTDM5w971iFwmw-iSyTDQQ4MyGL8-"))
+# Download the CSV file
+googledrive::drive_download(file = toy_files$id[toy_files$name=="NM-BR Toy dataset.csv"], 
+                            path = "drivedata/toy.csv",
+                            overwrite = T)
+# read in csv
+toydata = read.csv("drivedata/toy.csv")
 
+### Wrangle toy data ####
+## Group by project
+nm_toy <- toydata[which(toydata$Project=='nm'),]
 
+br_toy <- toydata[which(toydata$Project=='br'),]
 
